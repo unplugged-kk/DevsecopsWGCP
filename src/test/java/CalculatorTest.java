ce8f404af869a924b1d6c97a7a4ef1071166ef89
Kishore Kumar  Behera <kishore@Kishores-MacBook-Pro.local>
2023-10-24 13:17:49 +0530

    Added changes to populate code coverage in the sonarcloud

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+/*
+#Added a Java Test File for Demonstration of Code Coverage Percentage update on SonarCloud Dashboard
+*/
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+    public void verifyAdditionOne() {
+
+		Calculator calculator = new Calculator();
+        int sum = calculator.addition("5+2");
+        assertEquals(7, sum);
+    }
+	
+	@Test
+    public void verifyAdditionTwo() {
+
+		Calculator calculator = new Calculator();
+        int sum = calculator.addition("13+27");
+        assertEquals(40, sum);
+    }
+}
